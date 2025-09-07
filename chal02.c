91a0aebe26ccd0030bc63acf7762af9182f90bc8
a-oirgari <abdrahmanghali1998@gmail.com>
2025-09-07 16:53:12 +0200

    exo2 pour les strings

---
+++ chal02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char T[30];
+    int l = 0;
+    
+    printf("Enter a string: ");
+    scanf("%s", T);
+
+    for (int i = 0; T[i] != '\0'; i++) {
+        l++;
+    }
+    printf("La longueur du string est : %d\n", l);
+    return 0;
+}
